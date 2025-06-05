e5858f7d7edfb18868e1b3f19eec6eb4a29a3c4f
Herish <herish@Herishs-MacBook-Pro.local>
2025-06-05 17:31:33 +0530

    New Push

---
+++ Data/Data.js
@@ -0,0 +1,11 @@
+export class StaticData {
+  static baseUrl ='http://20.96.182.140:4000/'
+}
+
+export class LoginData {
+
+  static adminLogin = {
+    userName : "admin@nucleus.com",
+    password : '123456'
+  }
+}
---
+++ playwright.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests-examples',
+  reporter: 'html',
+  projects: [
+    {
+      name: 'Chromium',
+      use: {
+        browserName: 'chromium',
+        viewport: null, // Disable fixed viewport, will be maximized by the browser itself
+        launchOptions: {
+          args: ['--start-maximized'], // Start Chromium maximized
+        },
+      },
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox',
+        viewport: { width: 1792, height: 1120 }, // Simulate maximized window size
+      },
+    },
+    {
+      name: 'WebKit',
+      use: {
+        browserName: 'webkit',
+        viewport: { width: 1792, height: 1120 }, // Simulate maximized window size
+      },
+    },
+    {
+      name: 'Edge',
+      use: {
+        channel: 'msedge',
+        viewport: { width: 1792, height: 1120 }, // Simulate maximized window size
+      },
+    },
+  ],
+});
